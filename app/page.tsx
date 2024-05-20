3058f0e2df47501a4a0ebe7bb110a8f1ce0caa72
Amirreza <amirrezakrym.com>
2024-05-20 22:34:55 +0430

    feat(app): Added Contact and Slider sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+'use client'
 import Image from "next/image";
-import { FaArrowRight } from "react-icons/fa6";
-import localFont from 'next/font/local'
 import Link from "next/link";
-const titleFont = localFont({src : '../public/assets/Fonts/DMSerifDisplay-Regular.ttf'})
+import localFont from 'next/font/local'
+import Slider from "@/components/slider/slider";
+// icon
+import { FaArrowRight } from "react-icons/fa6";
+import { PiCompassTool,PiMagicWand,PiToolbox,PiPhoneFill} from "react-icons/pi";
 
+
+// font
+const titleFont = localFont({src : '../public/assets/Fonts/DMSerifDisplay-Regular.ttf'})
 
 export default function Home() {
   return (
@@ -11,7 +17,7 @@ export default function Home() {
    
 
     {/* grid image */}
-    <div className="xl:bg-grid xl:bg-center xl:bg-repeat-y fixed top-0 bottom-0 left-0 right-0 z-10"></div>
+    <div className="xl:bg-grid xl:bg-center xl:bg-repeat-y fixed top-0 bottom-0 left-0 right-0 z-[1]"></div>
     
     {/* home-banner */}
     <section className="bg-home-banner h-[640px] xl:h-[740px] bg-center lg:bg-cover bg-no-repeat bg-fixed md:rounded-bl-[150px] xl:rounded-bl-[290px] relative z-20">
@@ -19,7 +25,7 @@ export default function Home() {
       <div className="container  mx-auto h-full flex items-center text-center xl:justify-start">
 
         <div className="w-[567px] md:ml-5 flex flex-col items-center text-center md:text-left md:items-start">
-          <h1 className={`${titleFont.className} text-6xl mb-8`}>Let Your Home Be Unique</h1>
+          <h1 className={`${titleFont.className} text-4xl md:text-6xl mb-8`}>Let Your Home Be Unique</h1>
           <p className="mb-8"> There are many variations of the passages of lorem Ipsum from available,variations of the passages.</p>
           <button className="tracking-wide group mx-auto md:mx-0 bg-black text-white hover:text-black p-4 rounded-xl  after:bg-[#b88c5d] font-medium z-30 relative  after:-z-20 after:absolute after:h-1 after:w-1  after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 ">Get free estimation <FaArrowRight  className="inline group-hover:text-black duration-700 text-[#b88c5d]"/></button>
         </div>
@@ -27,10 +33,99 @@ export default function Home() {
       </div>
     </section>
 
+    {/* About Us */}
+    <section className="mt-20 relative z-20">
+      <div className="container mx-auto flex justify-center items-start">
+
+      <div className='grid grid-cols-1 lg:grid-cols-3 mx-auto text-black'>    
+
+              <div className="flex flex-col items-center justify-center mx-auto text-center">
+                <PiCompassTool  className="text-[#b88c5d] text-5xl mb-4 transition ease-in hover:scale-125"/>
+                <h3 className={`${titleFont.className} text-2xl mb-5`}>
+                  Project Planning
+                </h3>
+                <p className="mb-4 max-w-md mx-auto px-4 w-4/5">
+                There are many variations of the passages of lorem Ipsum from available, majority.</p>
+                <Link href={"#"} className='group transition-all duration-300 hover:text-[#b88c5d]'>
+                  Read more <FaArrowRight  className="inline text-[#b88c5d] transition-all duration-300 group-hover:text-black"/>
+                </Link>
+              </div>
+
+              <div className="flex my-8 lg:my-0 flex-col items-center justify-center mx-auto text-center">
+                <PiMagicWand   className="text-[#b88c5d] text-5xl mb-4 transition ease-in hover:scale-125"/>
+                <h3 className={`${titleFont.className} text-2xl mb-5`}>
+                  Gaining Materials
+                </h3>
+                <p className="mb-4 max-w-md mx-auto px-4 w-4/5">
+                There are many variations of the passages of lorem Ipsum from available, majority.</p>
+                <Link href={"#"} className='group transition-all duration-300 hover:text-[#b88c5d]'>
+                  Read more <FaArrowRight  className="inline text-[#b88c5d] transition-all duration-300 group-hover:text-black"/>
+                </Link>
+              </div>
+        
+              <div className="flex flex-col items-center justify-center mx-auto text-center">
+                <PiToolbox   className="text-[#b88c5d] text-5xl mb-4 transition ease-in hover:scale-125"/>
+                <h3 className={`${titleFont.className} text-2xl mb-5`}>
+                  Project Execution
+                </h3>
+                <p className="mb-4 max-w-md mx-auto px-4 w-4/5">
+                There are many variations of the passages of lorem Ipsum from available, majority.</p>
+                <Link href={"#"} className='group transition-all duration-300 hover:text-[#b88c5d]'>
+                  Read more <FaArrowRight  className="inline text-[#b88c5d] transition-all duration-300 group-hover:text-black"/>
+                </Link>
+              </div>
+       
+          </div>
+        
+        </div> 
+    </section>
 
 
+    {/* Contact US */}
+    <section className="mt-20 relative z-20">
+      <div className="container mx-auto ">
+          <div className="flex flex-col px-10 lg:flex-row text-center lg:text-left justify-between items-center gap-5">
 
-<div className="h-[3000px]"></div>
+            <div className=" order-2 lg:order-none lg:max-w-[410px] flex flex-col items-center lg:items-start gap-8">
+              <h2 className={`${titleFont.className} text-3xl md:text-4xl`}>
+                 We Create The Art Of Stylish Living Stylishly
+              </h2>
+              <p>It is a long established fact that a reader will be distracted by the of readable content of a page 
+                when lookings at its layouts the points of using 
+                that it has a more-or-less normal.
+              </p>
+              <div className="flex items-center justify-center lg:justify-start gap-3">
+                <div className="flex justify-center items-center bg-[#eddfd1] w-20 h-20 rounded-full">
+                  <PiPhoneFill  className="text-[#b88c5d] text-4xl "/>
+                </div>
+
+                <div className="text-left">
+                    <div className="text-xl md:text-2xl font-bold">09399671609</div>
+                    <div>Call Us Anytime</div>
+                </div>
+              </div>
+
+              <button className="tracking-wide group mx-auto md:mx-0 bg-black text-white hover:text-black p-4 rounded-xl  after:bg-[#b88c5d] font-medium z-30 relative  after:-z-20 after:absolute after:h-1 after:w-1  after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 ">Get free estimation <FaArrowRight  className="inline group-hover:text-black duration-700 text-[#b88c5d]"/></button>
+        
+            </div>
+
+
+            <div className="order-1 lg:order-none max-w-[453px] mx-auto lg:max-w-none lg:mx-0"><Image className="" alt="Contact" src={'/assets/Contact.png'} width={453} height={200}></Image></div>
+          </div>
+      </div>
+    </section>
+
+    {/* slider */}
+
+    <section className="mt-20 relative z-20">
+      <div className="container mx-auto bg-[#eddfd1] rounded-[3.5rem] p-5 py-20 pb-36">
+        <div className={`${titleFont.className} text-4xl md:text-6xl mb-8 text-center`}>What clients say</div>
+        <Slider/>
+      </div>
+    </section>
+
+<div className="h-[3000px]"></div>``
+
    </main>
   );
 }
---
+++ components/slider/slider.tsx
@@ -0,0 +1,121 @@
+'use client'
+import Image from 'next/image';
+import localFont from 'next/font/local'
+// Swiper
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import 'swiper/css';
+
+// font
+const titleFont = localFont({src : '../../public/assets/Fonts/DMSerifDisplay-Regular.ttf'})
+
+const Slider = () => {
+    return (  
+        <>
+        <Swiper
+      pagination={true} 
+      modules={[Pagination]}
+      spaceBetween={30}
+      slidesPerView={3}
+      breakpoints={{
+        0: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide >
+        <div className='flex flex-col justify-center items-center sm:items-start bg-white w-4/5 mx-auto sm:w-auto rounded-xl p-8 py-12'>
+        <div className='flex  gap-3 mb-5'>
+            <div><Image alt='hero' src={'/assets/hero/04.png'} width={70} height={70}></Image></div>
+            <div className='flex flex-col justify-center items-start '>
+                <h1 className={`${titleFont.className} text-xl md:text-2xl`}>Natasha Mith</h1>
+                <div className='text-sm'>Greenville, USA</div>
+            </div>
+        </div>
+        <p className='max-w-64 text-[14px] sm:text-[16px]'>Lorem Ipsum is simply dummy 
+        text of the typesetting industry. 
+        Ipsum has been.
+        </p>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+      <div  className='flex flex-col justify-center items-center sm:items-start bg-white w-4/5 mx-auto sm:w-auto rounded-xl p-8 py-12'>
+        <div className='flex justify-start gap-3 mb-5'>
+            <div><Image alt='hero' src={'/assets/hero/03.png'} width={70} height={70}></Image></div>
+            <div className='flex flex-col justify-center items-start '>
+                <h1 className={`${titleFont.className} text-xl md:text-2xl`}>Jessica White</h1>
+                <div className='text-sm'>Oak Ridge, USA</div>
+            </div>
+        </div>
+        <p className='max-w-64 text-[14px] sm:text-[16px]'>Lorem Ipsum is simply dummy 
+        text of the typesetting industry. 
+        Ipsum has been.
+        </p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide >
+      <div  className='flex flex-col justify-center items-center sm:items-start bg-white w-4/5 mx-auto sm:w-auto rounded-xl p-8 py-12'>
+        <div className='flex gap-3 mb-5'>
+            <div><Image alt='hero' src={'/assets/hero/02.png'} width={70} height={70}></Image></div>
+            <div className='flex flex-col justify-center items-start '>
+                <h1 className={`${titleFont.className} text-xl md:text-2xl`}>Mike Davis</h1>
+                <div className='text-sm'>Berlin, Germany</div>
+            </div>
+        </div>
+        <p className='max-w-64 text-[14px] sm:text-[16px]'>Lorem Ipsum is simply dummy 
+        text of the typesetting industry. 
+        Ipsum has been.
+        </p>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide >
+      <div  className='flex flex-col justify-center items-center sm:items-start bg-white w-4/5 mx-auto sm:w-auto rounded-xl p-8 py-12'>
+        <div className='flex gap-3 mb-5'>
+            <div><Image alt='hero' src={'/assets/hero/01.png'} width={70} height={70}></Image></div>
+            <div className='flex flex-col justify-center items-start '>
+                <h1 className={`${titleFont.className} text-xl md:text-2xl`}>Olivia Wilson</h1>
+                <div className='text-sm'>Blue Ridge, France</div>
+            </div>
+        </div>
+        <p className='max-w-64 text-[14px] sm:text-[16px]'>Lorem Ipsum is simply dummy 
+        text of the typesetting industry. 
+        Ipsum has been.
+        </p>
+        </div>
+      </SwiperSlide>
+
+      <SwiperSlide>
+      <div  className='flex flex-col justify-center items-center sm:items-start bg-white w-4/5 mx-auto sm:w-auto rounded-xl p-8 py-12'>
+        <div className='flex gap-3 mb-5'>
+            <div><Image alt='hero' src={'/assets/hero/01.png'} width={70} height={70}></Image></div>
+            <div className='flex flex-col justify-center items-start '>
+                <h1 className={`${titleFont.className} text-xl md:text-2xl`}>Olivia Wilson</h1>
+                <div className='text-sm'>Blue Ridge, France</div>
+            </div>
+        </div>
+        <p className='max-w-64 text-[14px] sm:text-[16px]'>Lorem Ipsum is simply dummy 
+        text of the typesetting industry. 
+        Ipsum has been.
+        </p>
+        </div>
+      </SwiperSlide>
+
+    </Swiper>
+        </>
+      );
+}
+ 
+export default Slider;
